6c38a38ad5d6517314090ed6aaeb9ac1ba8cfacb
hasan <mohdhasan7867214@gmail.com>
2025-11-15 23:53:13 +0530

    implemented a card UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,14 @@ function App() {
       <div className="parent">
         {Array(12).fill(null).map((_, index) => {
           return (
+            <div className="product-parent">
             <div key={index + 1} className="card">
+              <img className="product-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Derbys_Peter_Pan_peanut_butter_sample_blikje%2C_foto3.JPG/250px-Derbys_Peter_Pan_peanut_butter_sample_blikje%2C_foto3.JPG"
+                alt="Product Image" />
+            </div>
+            <p className="product-name">
+              Product {index + 1}
+            </p>
             </div>
           )
         })}
