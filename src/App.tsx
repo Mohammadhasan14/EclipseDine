5d7e8f1fd5a95c4c3ef6bab72b7e10c83351446b
hasan <mohdhasan7867214@gmail.com>
2025-11-17 22:52:56 +0530

    implemented a UI for scroll behavior of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="branding-section">
+        <img src="/images/bg-shop-img.jpeg" alt="" className="top-section-bg"/>
         <Navbar />
       </div>
       <div className="content-section">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+
+  }, []);
+
   return (
-    <div className="navbar-parent">
+    <div className={`navbar-parent ${scrolled ? 'navbar-scrolled' : ''}`}>
       <div className="navbar-brand-logo">
         <div className="navbar-brand-name">Eclipse Dine</div>
       </div>
